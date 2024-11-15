c8f63e49c96b8ef5cf346d4b7f9214a2d9c4c20c
Muhammed Althaf <Mdalthaf424@gmail.com>
2024-11-15 19:57:13 +0530

    fix

--- utils/generateAndSetCookie.js
+++ utils/generateAndSetCookie.js
@@ -7,6 +7,7 @@ export const generateAndSetCookies = (res, userId) => {
       res.cookie('token', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
+        sameSite:"None",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       });
   
