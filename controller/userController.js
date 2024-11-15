5c7477856e0d61eb40da7cd19a7cea3ee7d1d634
Muhammed Althaf <Mdalthaf424@gmail.com>
2024-11-15 22:59:12 +0530

    fix

--- controller/userController.js
+++ controller/userController.js
@@ -70,7 +70,7 @@ try {
 
 export const logout = async (req, res) => {
     try {
-      res.clearCookie("token", { httpOnly: true, secure: process.env.NODE_ENV==="production", sameSite: "Strict" });
+      res.clearCookie("token", { httpOnly: true, secure: process.env.NODE_ENV==="production",sameSite:"None" });
       res.status(200).json({ success: true, message: "Logout successful" });
     } catch (error) {
       res.status(500).json({ success: false, message: "Logout failed" });
