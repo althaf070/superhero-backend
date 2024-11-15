a926309263d300bac7ebefb52ffb10dc076df799
Muhammed Althaf <Mdalthaf424@gmail.com>
2024-11-15 19:50:01 +0530

    fix

--- index.js
+++ index.js
@@ -10,8 +10,18 @@ dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT || 3000
-
-app.use(cors({origin:'http://localhost:5173',credentials: true}))
+const allowedOrgins=['http://localhost:5173','https://justicebridge.vercel.app']
+app.use(cors({
+    origin: (origin, callback) => {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin || allowedOrgins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  }));
 app.use(express.json());    
 app.use(cookieParser())
 
