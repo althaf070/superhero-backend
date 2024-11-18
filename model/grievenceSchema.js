91986f308742f286fcce7aaea447269e5b03613a
Muhammed Althaf <Mdalthaf424@gmail.com>
2024-11-18 19:07:10 +0530

    fix

--- model/grievenceSchema.js
+++ model/grievenceSchema.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const grievenceSchema = new mongoose.Schema({
     userId:{
         type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
         required:true
     },
     grievanceType: {
